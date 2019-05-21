dfcd23dc524598cff8aa96b85ed33434d0b33e33
李传浩 <ALNY-AL@outlook.com>
2019-05-21 16:32:57 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,8 @@ module.exports = {
         '~/plugins/main.js',
     ],
     server: {
+        port: 3000,
+        host: '0.0.0.0',
         // port: 8080,
         // host: '192.168.3.107',
         // default: 80
